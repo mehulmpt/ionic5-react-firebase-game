31d9644aaf8a2b5aad0965f5089c00a8b36c62b9
Mehul Mohan <mehulmpt@gmail.com>
2020-05-22 04:21:40 +0530

    feature: Adding Google Sign In now

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './ExploreContainer.css';
+import React from 'react'
+import './ExploreContainer.css'
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
-};
+	return (
+		<div className="container">
+			<strong>Hello Ionic 5!</strong>
+			<p>This looks so awesome!</p>
+		</div>
+	)
+}
 
-export default ExploreContainer;
+export default ExploreContainer
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,24 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import React from 'react'
+import './styles.css'
+
+const Home: React.FC = () => {
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Login</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+				<IonHeader collapse="condense">
+					<IonToolbar>
+						<IonTitle size="large">Hello World!!</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+			</IonContent>
+		</IonPage>
+	)
+}
+
+export default Home
